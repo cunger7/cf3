56c2caf0601e55e4cf6dd1c91327e4b3b08f9111
Connor V.U <cunger7@gmail.com>
2019-02-01 03:56:21 -0600

    4.6 orders view and details added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.includes(:product).all
   end
   def show
+    @order = Order.find(params[:id])
   end
   def new
     @order = Order.new
